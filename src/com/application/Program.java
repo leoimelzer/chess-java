a1b1c52af0d6c4ec6a235a85fcd932ae7d9a2f2b
Leonardoismael <leonardoismael@insoft4.com>
2022-09-06 10:12:56 -0300

    Starting to implement Board and Piece

--- src/com/application/Program.java
+++ src/com/application/Program.java
@@ -1,11 +1,10 @@
 package com.application;
 
-import com.boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
-        Position pos = new Position(5,1);
-        System.out.println(pos);
+
+
     }
 }
